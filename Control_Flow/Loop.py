cad6bdf8a1310b49ff0fae756883a65a80b6500b
Dhruvinds <dhruvinsorathiya6464@gmail.com>
2023-06-07 14:57:30 +0530

    Loop Done!

--- Control_Flow/Loop.py
+++ Control_Flow/Loop.py
@@ -389,22 +389,22 @@
 
 # Match Case
 
-day = int(input("Enter a number: "))
+# day = int(input("Enter a number: "))
 
-match day:
-    case 1:
-        print("Sunday")
-    case 2:
-        print("Monday")
-    case 3:
-        print("Tuesday")
-    case 4:
-        print("Wednesday")
-    case 5:
-        print("Thursday")
-    case 6:
-        print("Friday")
-    case 7:
-        print("Saturday")
-    case _:
-        print("Holiday")
+# match day:
+#     case 1:
+#         print("Sunday")
+#     case 2:
+#         print("Monday")
+#     case 3:
+#         print("Tuesday")
+#     case 4:
+#         print("Wednesday")
+#     case 5:
+#         print("Thursday")
+#     case 6:
+#         print("Friday")
+#     case 7:
+#         print("Saturday")
+#     case _:
+#         print("Holiday")
