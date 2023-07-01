f5ab776e7a64c0b5f258fda625b6d0c1b8969fdb
Dhruvinds <dhruvinsorathiya6464@gmail.com>
2023-07-01 14:19:31 +0530

    Operator with strings

--- String.py
+++ String.py
@@ -14,3 +14,67 @@
 # print(s2[-1])
 # print(s4)
 
+# Operators on String
+#1. Concatenation 2. Repetition 3. Indexing 4.Slicing 5. in 6. Not in
+
+# 1. Concatenation
+# s1 = "Hello"
+# s2 = "World"
+
+# s3 = s1 + " " + s2
+# print(s3)
+
+# s1 = "How " + "Are " + "You"
+# print(s1)
+
+# s1 = "Hello" + str(15)
+# print(s1)
+
+# 2. Repetition
+
+# s1 = "Dhruvin"
+# s2 = s1 * 4
+# print(s2)
+
+# 3. Indexing
+# Sequence or array of character
+# s1 = "Hello World"
+# for x in s1 :
+#     print(x)
+
+# Range based indexing
+# s2 = "How Are You!"
+# for i in range(0, len(s2)) :
+#     print(s2[i])
+
+# Reverse String Printing
+
+# s1 = "Hello World"
+# print(len(s1))
+
+# for i in range(len(s1)-1, -1, -1) :
+#     print(s1[i])
+
+# Printing of skipping 1 number
+
+# for i in range(0, len(s1), 2) :
+#     print(s1[i])
+
+# 4. Slicing
+#string[start:end:step] optional
+
+# s1 = "Hello World"
+
+# print(s1[0:12:1])
+# print(s1[::1])
+# print(s1[0:len(s1):1])
+# print(s1[-1:-len(s1)-1:-1])
+
+#5 IN
+# s1 = "Dhruvin Sorathiya"
+# s2 = "D" in s1
+# print(s2)
+
+#6 NOT IN
+# s3 = "H" not in s1
+# print(s3)
