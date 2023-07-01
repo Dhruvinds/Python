2debb4806d214f25678acf6eff6e770c6da178c2
Dhruvinds <dhruvinsorathiya6464@gmail.com>
2023-07-01 12:03:41 +0530

    String basics

---
+++ String.py
@@ -0,0 +1,16 @@
+# Variable and gives direct value to the string that means it works like literals
+
+# s1 = 'Hello'
+# s2 = "Hello"
+# # s3 = 'John's'
+# s3 = "John's"
+# s4 = '''Hello
+#         How Are You'''
+
+# for x in s4 :
+#     print(x)
+
+# print(s2[1])
+# print(s2[-1])
+# print(s4)
+
